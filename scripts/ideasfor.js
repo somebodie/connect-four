eb68eb6220963ef49620175aea99a8e76cd74cc8
s0m3b0di3 <s00m3b0di3@gmail.com>
2016-12-23 13:00:06 -0500

    made seperate scripts to allow ideas to play with and still in need of a board to compute functions as well as styling

---
+++ scripts/ideasfor.js
@@ -0,0 +1,30 @@
+//
+// var yellow : [21 spaces]
+// var red: [21 spaces]
+//
+//
+//
+// playersWin(function(event)) {
+//
+// }
+//
+// validMove(event){
+//   case expression:
+//
+//     break;
+// }
+//
+// winningMove(){
+//
+// }
+//
+// atVictoryMessage (Player Wins!)
+// alert (play again?)
+//
+// AI(easy : hard) {
+//   Easy - Random
+//   Hard - Tries to stop win;
+// }
+//
+// column (makes move)
+// row (makes validMove)
--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,3 @@
 $(document).ready(function() {
   console.log( 'ready!');
 })
-//
-// var yellow : [21 spaces]
-// var red: [21 spaces]
-//
-//
-//
-// playersWin(function(event)) {
-//
-// }
-//
-// validMove(event){
-//   case expression:
-//
-//     break;
-// }
-//
-// winningMove(){
-//
-// }
-//
-// atVictoryMessage (Player Wins!)
-// alert (play again?)
-//
-// AI(easy : hard) {
-//   Easy - Random
-//   Hard - Tries to stop win;
-// }
-//
-// column (makes move)
-// row (makes validMove)
