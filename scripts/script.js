6ff116341b94e2b24b2bc057a2f04fc4752e220c
s0m3b0di3 <s00m3b0di3@gmail.com>
2016-12-23 23:01:48 -0500

    still hoping to get the board to be fleshed out but broke on to something that may help with reading the boardcalled flex-flow still cleaning out the thought of the board, writing down a lot to help get there.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
   console.log( 'ready!');
 })
+
+function onStart(event) {
+
+}
+
+function makeMove(event) {
+
+}
+
+function validMove(event) {
+
+}
+
+function winningMove(event) {
+
+}
+
+function victoryMessage(event) {
+
+}
+
+function AI(event) {
+  easy
+  hard
+}
