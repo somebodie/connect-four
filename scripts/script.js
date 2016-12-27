48c21acd22811017566af2804160347bc204e84a
s0m3b0di3 <s00m3b0di3@gmail.com>
2016-12-26 22:00:13 -0500

    figured out a starting idea for game piece similiar to progress bar expect moving postion of of piece down, still determining how to make sure of no overlapping

--- scripts/script.js
+++ scripts/script.js
@@ -30,3 +30,18 @@ function AI(event) {
 function whenMove(event) {
   // http://www.w3schools.com/TAgs/canvas_fill.asp
 }
+
+function onClick() {
+  // drawProgressBars: function(timerValue){
+  // //// MVP
+  // var timeElapsed = 100 - timerValue;
+  // document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
+  // //// REACH
+  // // var progress = 100 - timerValue;
+  // // var bars = document.getElementsByClassName('progress-bar');
+  // // for (var i = 0; i < bars.length; i++) {
+  // //   bars[i].style.width = progress + '%';
+  // // }
+}
+// source progress bar sample to make piece move
+// gamePiece.style.postion.top +(howmanyitneeds to fall in place)
