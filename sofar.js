436cb4d1957a1c685abd789a4348f490c061954b
s0m3b0di3 <s00m3b0di3@gmail.com>
2016-12-30 18:44:37 -0500

    looking to clean up and dry what I have to get some functions in, erased css as I know its where I need it to be just still unsure how to show it and make seen

---
+++ sofar.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  console.log( 'ready!');
+})
+
+var playerOne = [];
+var playerTwo = [];
+var [][] board = { };
+var activePlayer = 0
+function onStart(event) {
+// game piece loads
+}
+
+function makeMove(event) {
+// player picks column and it allows for player to make move in bottomw row
+
+}
+
+function validMove(event) {
+
+}
+
+function winningMove(event) {
+
+}
+
+function victoryMessage(event) {
+
+}
+
+function AI(event) {
+  easy
+  hard
+}
+
+function whenMove(event) {
+  // http://www.w3schools.com/TAgs/canvas_fill.asp
+}
+
+function onClick() {
+  // drawProgressBars: function(timerValue){
+  // //// MVP
+  // var timeElapsed = 100 - timerValue;
+  // document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
+  // //// REACH
+  // // var progress = 100 - timerValue;
+  // // var bars = document.getElementsByClassName('progress-bar');
+  // // for (var i = 0; i < bars.length; i++) {
+  // //   bars[i].style.width = progress + '%';
+  // // }
+}
+// source progress bar sample to make piece move
+// gamePiece.style.postion.top (or bottom and negative)+(howmanyitneeds to fall in place)
+
+after player makes move toggleclass move to other player
+
+// IDEA: swithcing players toogle class function
+
+// IDEA: $('.div this').html(makeMove[activePlayer]);
+
+// IDEA: $('startButton').hide() new game and rest.show() rest button
+
+// IDEA: plus reverse
