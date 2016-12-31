3641f539cd076f785793372de169b5e678816fee
s0m3b0di3 <s00m3b0di3@gmail.com>
2016-12-30 19:41:21 -0500

    filled in things I want to happen but not function as of yet, added in codes for what I know what I want now

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,57 @@ $(document).ready(function() {
   console.log( 'ready!');
 })
 
-var playerOne = red;
-var playerTwo = black;
+var redPlayer = [];
+var blackPlayer = [];
 // IDEA: var [][] board = { };
+
+var resetGame =
+
+var gameOver =
+
+var coinDrop = function() {
+  // manipulate css to drop piece .style
+}
+
+var currentPlayer = function(event) {
+
+}
+
+var takesTheBoard = function(event) {
+// alternate players
+}
+
+var isValidMove = function(event) {
+// IDEA: case switch
+// IDEA: reads rows to stop moves .hasClass
+}
+
+if (gamOver)
+
+var onClickBoardSpace = function(event) {
+  if (isValidMove) {
+    return currentPlayer.takesTheBoard
+  }
+}
+// IDEA: make move reads column or appends  .hasClass
+
+var makeMove = function() {
+
+}
+
+var onClickNewGame = function(event) {
+  return resetGame
+}
+
+var victoryMessage = function() {
+
+}
+
+// Possible AI
+var easy = function() {
+
+}
+
+var hard = function() {
+
+}
