4d27d882243faaab4d9e8fbf183f9c68c862ec0e
s0m3b0di3 <s00m3b0di3@gmail.com>
2016-12-30 21:49:03 -0500

    organized codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  console.log( 'ready!');
+    console.log('ready!');
 })
 
 var redPlayer = [];
@@ -9,12 +9,16 @@ var blackPlayer = [];
 
 // IDEA: var [][] board = { };
 
-var resetGame =
+var resetGame = function() {
 
-var gameOver =
+}
+
+var gameOver = function() {
+
+}
 
 var startGame = function() {
-  // IDEA: $('startButton').hide() new game and rest.show() rest button
+    // IDEA: $('startButton').hide() new game and rest.show() rest button
 }
 
 var onStart = function() {
@@ -26,54 +30,51 @@ var onClick = function() {
 }
 
 var coinDrop = function() {
-  // manipulate css to drop piece .style
-  // IDEA:
-  // drawProgressBars: function(timerValue){
-  // //// MVP
-  // var timeElapsed = 100 - timerValue;
-  // document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
-  // //// REACH
-  // // var progress = 100 - timerValue;
-  // // var bars = document.getElementsByClassName('progress-bar');
-  // // for (var i = 0; i < bars.length; i++) {
-  // //   bars[i].style.width = progress + '%';
-  // // }
-}
-// source progress bar sample to make piece move
-// gamePiece.style.postion.top (or bottom and negative)+(howmanyitneeds to fall in place)
-
-}
-
+        // manipulate css to drop piece .style
+        // IDEA:
+        // drawProgressBars: function(timerValue){
+        // //// MVP
+        // var timeElapsed = 100 - timerValue;
+        // document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
+        // //// REACH
+        // // var progress = 100 - timerValue;
+        // // var bars = document.getElementsByClassName('progress-bar');
+        // // for (var i = 0; i < bars.length; i++) {
+        // //   bars[i].style.width = progress + '%';
+        // // }
+    }
+    // source progress bar sample to make piece move
+    // gamePiece.style.postion.top (or bottom and negative) (howmanyitneeds to fall in place)
 var currentPlayer = function(event) {
 
 }
 
 var takesTheBoard = function(event) {
-// alternate players .toggleclass
+    // alternate players .toggleclass
 }
 
 var isValidMove = function(event) {
-// IDEA: case switch //   case expression:
-//
-//     break;
-// IDEA: reads rows to stop moves .hasClass
+    // IDEA: case switch //   case expression:
+    //
+    //     break;
+    // IDEA: reads rows to stop moves .hasClass
 }
 
-if (gamOver)
+// if (gamOver) {}
 
 var onClickBoardSpace = function(event) {
-  if (isValidMove) {
-    return currentPlayer.takesTheBoard
-  }
-}
-// IDEA: make move reads column or appends  .hasClass
+        if (isValidMove) {
+            return currentPlayer.takesTheBoard
+        }
+    }
+    // IDEA: make move reads column or appends  .hasClass
 
 var makeMove = function() {
-// IDEA: $('.div this').html(makeMove[activePlayer]);
+    // IDEA: $('.div this').html(makeMove[activePlayer]);
 }
 
 var onClickNewGame = function(event) {
-  return resetGame
+    return resetGame
 }
 
 var winningMove = function(event) {
@@ -85,15 +86,15 @@ var playersWin = function() {
 }
 
 var victoryMessage = function() {
-  // atVictoryMessage (Player Wins!)
-  // alert (play again?)
+    // atVictoryMessage (Player Wins!)
+    // alert (play again?)
 }
 
 // Possible AI
 var easy = function() {
-//  - Random
+    //  - Random
 }
 
 var hard = function() {
-//   - Tries to stop win;
+    //   - Tries to stop win;
 }
