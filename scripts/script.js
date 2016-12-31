5c4808016baae8163357dcf4bf73259cdbe4ebbc
s0m3b0di3 <s00m3b0di3@gmail.com>
2016-12-31 18:03:41 -0500

    lost css for players however got piece to show after start game

--- scripts/script.js
+++ scripts/script.js
@@ -13,15 +13,27 @@ var startGame =
   $( ".start" ).click(function() {
     console.log("starting!");
     // message can be placed that says log
+    $('#gamePiece').addClass('redPlayer');
   });
 
-var onStartGame =
-  // when player clicks start button red piece shows up
-  $('.start').click(function(){
-      $("div:last").addClass("redplayer");
-  });
-  // alternatePlayer;
-   // IDEA: maybe in advance allow to pick color
+var selections =
+  // when player clicks makes a selection button diffucutlty changes
+//   switch($('level')) {
+//     case pvp:
+//         alternatePlayer;
+//         break;
+//     case easy:
+//        $('column')Math.Random(div:column);
+//         break;
+//     case hard:
+//         code block
+//         break;
+//     default:
+//         console.log('selection made')
+// }
+      // IDEA: maybe in advance allow to pick color
+  // });
+
 
 // var alternatePlayer =
 //     $('.redplayer').toggleClass('.yellowPlayer');
@@ -30,20 +42,24 @@ var onStartGame =
 // slideDown()
 // when a player picks a column piece drops and then alternatePlayer
 
-var makeMove = function() {
-  // if there is a next prompt
-  if (currentPrompt < prompts.length) {
-	//put current prompt in all html elements with class
-	$('.prompt').html(prompts[currentPrompt]);
-	// move the next prompt into variable currentPrompt
-	currentPrompt = currentPrompt + 1;
-  }
-  // or else if we're at the end of the array
-  else {
-    // put a new message into the html
-    $('.prompt').html("that's all for now!");
-    }
-}
-$("column").click(function(){
-    $("div:last").addClass("activePlayer");
-});
+// var makeMove = function() {
+//   $("column").click(function(){
+//       $("div:last").addClass("activePlayer");
+      // alternatePlayer;
+//   });
+// }
+
+// var isValidMove =
+//   // if there is a next prompt
+//   if (currentPrompt < prompts.length) {
+// 	//put current prompt in all html elements with class
+// 	$('.prompt').html(prompts[currentPrompt]);
+// 	// move the next prompt into variable currentPrompt
+// 	currentPrompt = currentPrompt + 1;
+//   }
+//   // or else if we're at the end of the array
+//   else {
+//     // put a new message into the html
+//     $('.prompt').html("that's all for now!");
+//     }
+// }
