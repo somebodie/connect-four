0a13cb737785aa79a01a1cf721f5be9c29c06dc2
s0m3b0di3 <s00m3b0di3@gmail.com>
2017-01-04 20:48:22 -0500

    making sure everything is uptodate

--- scripts/script.js
+++ scripts/script.js
@@ -2,24 +2,24 @@ $(document).ready(function() {
     console.log('ready!');
 })
 
-var players = {
-    1: 'redPlayer'
-    2: 'yellowPlayer'
-};
+// var players = {
+//     1: 'redPlayer'
+//     2: 'yellowPlayer'
+// };
 
-var columns =
-
-$('#6'): [1, 2, 3, 4, 5, 6, 7];
-$('#5'): [1, 2, 3, 4, 5, 6, 7];
-$('#4'): [1, 2, 3, 4, 5, 6, 7];
-$('#3'): [1, 2, 3, 4, 5, 6, 7];
-$('#2'): [1, 2, 3, 4, 5, 6, 7];
-$('#1'): [1, 2, 3, 4, 5, 6, 7];
-
-var pickingSpace =
-
-    columns.pop.last
-  }
+// var columns =
+//
+// $('#6'): [1, 2, 3, 4, 5, 6, 7];
+// $('#5'): [1, 2, 3, 4, 5, 6, 7];
+// $('#4'): [1, 2, 3, 4, 5, 6, 7];
+// $('#3'): [1, 2, 3, 4, 5, 6, 7];
+// $('#2'): [1, 2, 3, 4, 5, 6, 7];
+// $('#1'): [1, 2, 3, 4, 5, 6, 7];
+//
+// var pickingSpace =
+//
+//     columns.pop.last
+//   }
 
 var startGame =
     $(".start").click(function() {
@@ -28,7 +28,7 @@ var startGame =
         console.log("starting!");
     });
 
-var mouseMoves =
+var mouseMoves 
 
     // var alternatePlayer =
     //     // switch from redPlayer to yellowPlayer
