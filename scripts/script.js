1b930a71c0c77e73ca09c183c1906a6c179b1916
s0m3b0di3 <s00m3b0di3@gmail.com>
2017-01-08 16:45:07 -0500

    updating

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
     console.log('ready!');
 });
 
+var board = {
+    row1: [null, null, null, null, null, null, null],
+    row2: [null, null, null, null, null, null, null],
+    row3: [null, null, null, null, null, null, null],
+    row4: [null, null, null, null, null, null, null],
+    row5: [null, null, null, null, null, null, null],
+    row6: [null, null, null, null, null, null, null],
+}
+
 var startGame =
     $(".start").click(function() {
         // gamePiece shows red on startGame
@@ -10,37 +19,52 @@ var startGame =
     });
 
 var column = $('div.columns').click(function() {
-    fillingSpace();
-    alternatePlayer();
-});
+    // readColumn($(this));
+    // fillingSpace();
+    // alternatePlayer();
+var clickedColumn = this;
+var rowsInColumn = this.children;
+// var rows = column[0].children;
+for (var i = 0; i < rowsInColumn.length; i++) {
+  switch ($(rowsInColumn[i])) {
+    case !$(rowsInColumn[i]).attr('background-color, yellow'):
+      $(rowsInColumn[i]).addClass('yellowPlayer');
+      break;
+    default: console.log('read');
 
-var fillingSpace = function() {
-    if ($('#gamePiece').hasClass('yellowPlayer')) {
-        .css('background-color', 'yellow');
-        console.log('yellow');
-    } else if ($('#gamePiece').hasClass('redPlayer')) {
-        {
-            .css('background-color', 'red');
-            console.log('red');
-        }
-    }
-};
+  }
 
-var readColumn = function(column) {
-    var rows = column[0].children;
-    for (var i = 0; i < rows.length; i++) {
-        if (!rows[i].classList.contains('yellowPlayer') || !rows[i].classList.contains('redPlayer')) {
-            console.log($(rows[i]));
-            break;
-        }
-    }
+      }
+}
 };
+// var fillingSpace = function() {
+//     // var gamePiece = $('#gamePiece');
+//     if ($('#gamePiece').hasClass('yellowPlayer')) {
+//         rows[i].css('background-color', 'yellow');
+//         console.log('yellow');
+//     } else if ($('#gamePiece').hasClass('redPlayer')) {
+//         {
+//             rows[i].css('background-color', 'red');
+//             console.log('red');
+//         }
+//     }
+// };
 
-// when player picks a column the lowest column has id filled with player then alternatePlayer
-var alternatePlayer = function() {
-    $('.redPlayer').toggleClass('yellowPlayer');
-    console.log('Player Switched!');
-};
+// var readColumn = function(column) {
+    // var rows = column[0].children;
+    // for (var i = 0; i < rows.length; i++) {
+    //     if (!rows[i].classList.contains('yellowPlayer') || !rows[i].css("background-color", "red")) {
+    //         console.log($(rows[i]));
+    //         break;
+        // }
+    // }
+// };
+//
+// // when player picks a column the lowest column has id filled with player then alternatePlayer
+// var alternatePlayer = function() {
+//     $('.redPlayer').toggleClass('yellowPlayer');
+//     console.log('Player Switched!');
+// };
 
 
 // var checkForWin
